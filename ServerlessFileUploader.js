3a5df28bf1cc355067d085a2259e52ec6d01db54
ryanchandler <ryan.chandler.35@gmail.com>
2020-10-23 05:32:54 -0500

    initial commit

---
+++ ServerlessFileUploader.js
@@ -0,0 +1,39 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+
+    // Extract file content
+    let fileContent = event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body;
+
+    // Generate file name from current timestamp
+    let fileName = `${Date.now()}`;
+
+    // Determine file extension
+    let contentType = event.headers['content-type'] || event.headers['Content-Type'];
+    let extension = contentType ? mime.extension(contentType) : '';
+
+    let fullFileName = extension ? `${fileName}.${extension}` : fileName;
+
+
+    // Upload the file to S3
+    try {
+        let data = await s3.putObject({
+            Bucket: "alfred-13",
+            Key: fullFileName,
+            Body: fileContent,
+            Metadata: {}
+        }).promise();
+
+
+
+        console.log("Successfully uploaded file", fullFileName);
+        return "Successfully uploaded";
+
+    } catch (err) {
+        console.log("Failed to upload file", fullFileName, err);
+        throw err;
+    };
+
+
+};
